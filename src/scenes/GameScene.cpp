73c68ad5403d84abac770e709a1b7c66c0c58a6d
Gonzalo Martín Villaescusa <gonzalo.villaescusa@live.u-tad.com>
2019-03-31 01:42:16 +0100

    Addes asserts in Game and Menu scenes

--- src/scenes/GameScene.cpp
+++ src/scenes/GameScene.cpp
@@ -8,6 +8,7 @@
 CWorld* g_pWorld;
 
 void CGameScene::init() {
+	ASSERT(g_pMenuManager && g_pWorld);
 	g_pMenuManager->deactivateMenu();
 	g_pWorld = NEW(CWorld);
 	g_pWorld->init(m_uLevel);
@@ -18,14 +19,17 @@ CGameScene::~CGameScene() {
 }
 
 void CGameScene::processInput() const {
+	ASSERT(g_pInputManager);
 	g_pInputManager->processInput();
 }
 
 void CGameScene::run(float _fDeltaTime) const {
+	ASSERT(g_pMenuManager && g_pWorld);
 	g_pMenuManager->run(_fDeltaTime);
 	g_pWorld->run(_fDeltaTime);
 }
 
 void CGameScene::render() const {
+	ASSERT(g_pGraphicEngine);
 	g_pGraphicEngine->render();
 }
--- src/scenes/MenuScene.cpp
+++ src/scenes/MenuScene.cpp
@@ -5,17 +5,21 @@
 #include "gui/MenuManager.h"
 
 void CMenuScene::init() {
+	ASSERT(g_pMenuManager);
 	g_pMenuManager->activateMenu(CMenuManager::EMainMenu);
 }
 
 void CMenuScene::processInput() const {
+	ASSERT(g_pInputManager);
 	g_pInputManager->processInput();
 }
 
 void CMenuScene::run(float _fDeltaTime) const {
+	ASSERT(g_pMenuManager);
 	g_pMenuManager->run(_fDeltaTime);
 }
 
 void CMenuScene::render() const {
+	ASSERT(g_pGraphicEngine);
 	g_pGraphicEngine->render();
 }
