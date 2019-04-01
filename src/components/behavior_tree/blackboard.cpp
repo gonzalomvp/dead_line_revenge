eed8a12b4618cec7651d98eb6a2e97ecda813ea0
Gonzalo Martín Villaescusa <gonzalomvp@hotmail.com>
2019-04-01 19:50:50 +0200

    Blackboard cleanup finished

--- src/components/behavior_tree/blackboard.cpp
+++ src/components/behavior_tree/blackboard.cpp
@@ -1,5 +1,4 @@
 #include "common/stdafx.h"
-
 #include "Blackboard.h"
 
 CBlackboard::~CBlackboard() {
@@ -8,75 +7,80 @@ CBlackboard::~CBlackboard() {
 	}
 }
 
-void CBlackboard::setValueVec2(TKey key, const vec2& _vValue) {
-	m_mapVec2[key] = _vValue;
+void CBlackboard::setValueVec2(std::string _sKey, const vec2& _vValue) {
+	m_mapVec2[_sKey] = _vValue;
 }
 
-void CBlackboard::setValueInt(TKey key, int _iValue) {
-	m_mapInt[key] = _iValue;
+void CBlackboard::setValueInt(std::string _sKey, int _iValue) {
+	m_mapInt[_sKey] = _iValue;
 }
 
-void CBlackboard::setValueFloat(TKey key, float _fValue) {
-	m_mapFloat[key] = _fValue;
+void CBlackboard::setValueFloat(std::string _sKey, float _fValue) {
+	m_mapFloat[_sKey] = _fValue;
 }
 
-void CBlackboard::setValueEntity(TKey key, CEntity* _pValue) {
+void CBlackboard::setValueEntity(std::string _sKey, CEntity* _pValue) {
 	ASSERT(_pValue);
 
-	if (m_mapEntity.count(key) > 0 && m_mapEntity[key]) {
-		m_mapEntity[key]->unregisterToDestroy(this);
-	}
+	clearValueEntity(_sKey);
 
-	m_mapEntity[key] = _pValue;
+	m_mapEntity[_sKey] = _pValue;
 	_pValue->registerToDestroy(this);
 }
 
-bool CBlackboard::getValueVec2(TKey key, vec2& vValue_) {
+bool CBlackboard::getValueVec2(std::string _sKey, vec2& vValue_) {
 	bool bRet = false;
-	auto it = m_mapVec2.find(key);
-	if (it != m_mapVec2.end())
-	{
+	if (m_mapVec2.count(_sKey) > 0) {
 		bRet = true;
-		vValue_ = m_mapVec2[key];
+		vValue_ = m_mapVec2[_sKey];
 	}
-
 	return bRet;
 }
 
-bool CBlackboard::getValueInt(TKey key, int& iValue_) {
+bool CBlackboard::getValueInt(std::string _sKey, int& iValue_) {
 	bool bRet = false;
-	auto it = m_mapInt.find(key);
-	if (it != m_mapInt.end())
-	{
+	if (m_mapInt.count(_sKey) > 0) {
 		bRet = true;
-		iValue_ = m_mapInt[key];
+		iValue_ = m_mapInt[_sKey];
 	}
-
 	return bRet;
 }
 
-bool CBlackboard::getValueFloat(TKey key, float& fValue_) {
+bool CBlackboard::getValueFloat(std::string _sKey, float& fValue_) {
 	bool bRet = false;
-	auto it = m_mapFloat.find(key);
-	if (it != m_mapFloat.end())
-	{
+	if (m_mapFloat.count(_sKey) > 0) {
 		bRet = true;
-		fValue_ = m_mapFloat[key];
+		fValue_ = m_mapFloat[_sKey];
 	}
-
 	return bRet;
 }
 
-bool CBlackboard::getValueEntity(TKey key, CEntity*& pValue_) {
+bool CBlackboard::getValueEntity(std::string _sKey, CEntity*& pValue_) {
 	bool bRet = false;
-	auto it = m_mapEntity.find(key);
-	if (it != m_mapEntity.end())
-	{
+	if (m_mapEntity.count(_sKey) > 0) {
 		bRet = true;
-		pValue_ = m_mapEntity[key];
+		pValue_ = m_mapEntity[_sKey];
 	}
-
 	return bRet;
+}
+
+void CBlackboard::clearValueVec2(std::string _sKey) {
+	m_mapVec2.erase(_sKey);
+}
+
+void CBlackboard::clearValueInt(std::string _sKey) {
+	m_mapInt.erase(_sKey);
+}
+
+void CBlackboard::clearValueFloat(std::string _sKey) {
+	m_mapFloat.erase(_sKey);
+}
+
+void CBlackboard::clearValueEntity(std::string _sKey) {
+	if (m_mapEntity.count(_sKey) > 0 && m_mapEntity[_sKey]) {
+		m_mapEntity[_sKey]->unregisterToDestroy(this);
+	}
+	m_mapEntity.erase(_sKey);
 }
 
 void CBlackboard::onEntityDestroyed(CEntity* _pEntity) {
--- src/components/behavior_tree/blackboard.h
+++ src/components/behavior_tree/blackboard.h
@@ -9,27 +9,30 @@ class CEntity;
 
 class CBlackboard : public CEntity::IListener {
 public:
-	typedef std::string TKey;
-
 	CBlackboard() {}
 	~CBlackboard();
 
-	void setValueVec2(TKey key, const vec2& _vValue);
-	void setValueInt(TKey key, int _iValue);
-	void setValueFloat(TKey key, float _fValue);
-	void setValueEntity(TKey key, CEntity* _pValue);
+	void setValueVec2  (std::string _sKey, const vec2& _v2Value);
+	void setValueInt   (std::string _sKey, int _iValue);
+	void setValueFloat (std::string _sKey, float _fValue);
+	void setValueEntity(std::string _sKey, CEntity* _pValue);
 
-	bool getValueVec2(TKey key, vec2& vValue_);
-	bool getValueInt(TKey key, int& iValue_ );
-	bool getValueFloat(TKey key, float& fValue_);
-	bool getValueEntity(TKey key, CEntity*& pValue_);
+	bool getValueVec2  (std::string _sKey, vec2& v2Value_);
+	bool getValueInt   (std::string _sKey, int& iValue_ );
+	bool getValueFloat (std::string _sKey, float& fValue_);
+	bool getValueEntity(std::string _sKey, CEntity*& pValue_);
+
+	void clearValueVec2  (std::string _sKey);
+	void clearValueInt   (std::string _sKey);
+	void clearValueFloat (std::string _sKey);
+	void clearValueEntity(std::string _sKey);
 
 	// CEntity::IListener
 	virtual void onEntityDestroyed(CEntity* _pEntity) override;
 
 private:
-	std::map<TKey, int>		    m_mapInt;
-	std::map<TKey, float>		m_mapFloat;
-	std::map<TKey, vec2>		m_mapVec2;
-	std::map<TKey, CEntity*>	m_mapEntity;
+	std::map<std::string, int>      m_mapInt;
+	std::map<std::string, float>    m_mapFloat;
+	std::map<std::string, vec2>     m_mapVec2;
+	std::map<std::string, CEntity*>	m_mapEntity;
 };
